f243fffb933103a58a0898e3e9b84f689924bf60
pavan <pavankmr1902@gmail.com>
2025-03-29 10:47:17 +0530

    added home redirect funtionality

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/login",
+        permanent: true, // 301 redirect (SEO friendly)
+      },
+    ];
+  },
 };
 
 export default nextConfig;
